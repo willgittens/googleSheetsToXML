e376fcc72e48fa169114ef1bc0533e047884c533
Will Gittens <contato@fwweb.com.br>
2017-10-11 12:52:27 -0300

    Version 1.0

---
+++ GoogleSheetsToXML.class.php
@@ -0,0 +1,88 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+define( "API_KEY","HERE-YOUR-GOOGLE-API-CONSOLE-KEY" );
+define( "API_NAME","HERE-THE-NAME-OF-YOUR-API-PROJECT" );
+
+define( "ID_SPREEDSHEET","HERE-THE-ID-OF-SPREEDSHEET" );
+define( "RANGE_SPREEDSHEET","A:E" );
+
+class GoogleSheetsToXML{
+
+		private $resultSheet;
+		private $resultCells;
+
+		private function checkAuth() {
+					$client = new Google_Client();
+					$client->setApplicationName(API_NAME);
+					$client->setDeveloperKey(API_KEY);
+					return $client;
+		}
+
+		private function arrayResult( $tab ){
+					$client = $this->checkAuth();
+					$service = new Google_Service_Sheets($client);
+
+					$spreadsheetId = ID_SPREEDSHEET;
+					$range = $tab."!".RANGE_SPREEDSHEET;
+
+					$response = $service->spreadsheets_values->get($spreadsheetId, $range);
+					$values = $response->getValues();
+
+					$this->resultSheet = array_reverse($values);
+					array_pop( $this->resultSheet );
+					$this->resultSheet = array_reverse( $this->resultSheet );
+
+					$this->resultCells = $values[0];
+		}
+
+		public function generateXML( $tabSpreedSheet ){
+
+					$this->arrayResult( $tabSpreedSheet );
+
+					$count = 0;
+
+					$result = '<'.$tabSpreedSheet.'>';
+
+					foreach ( $this->resultSheet as $key ) {
+						
+							$result .= '<item>';
+
+										foreach ( $this->resultCells as $key2 ) {
+												
+												$result .= '<'.$key2.'>';
+												
+												if( strpos( $key[$count], "<" ) !== false || strpos( $key[$count], ">" ) !== false || strpos( $key[$count], "/" ) !== false ){
+
+													$result .= "<![CDATA[";
+
+												}	
+													$result .= $key[$count];
+
+												if( strpos( $key[$count], "<" ) !== false || strpos( $key[$count], ">" ) !== false || strpos( $key[$count], "/" ) !== false ){
+
+													$result .= "]]>";
+
+												}
+
+												$result .= '</'.$key2.'>';
+
+												$count++;
+
+										}	
+
+							$result .= '</item>';	
+							$count = 0;
+
+					}
+
+					$result .= '</'.$tabSpreedSheet.'>';
+
+				    header('Access-Control-Allow-Origin: *'); 
+				    header("Content-type: text/xml; charset=utf-8");
+				    echo $result;					
+
+		}	
+
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+// Welcome to GoogleSheetsToXML
+// The easiest way to transform the tabs of your Google spreadsheet into an XML file.
+
+// You can see the full documentation on Github:
+// https://github.com/willgittens/googleSheetsToXML
+
